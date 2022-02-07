d4e594e97fd70436a87a47b6ff7bab7ad13f4cac
Cole Rossman <colerossman99@gmail.com>
2022-02-07 15:17:02 -0800

    animals.js variables done

--- animals/animals.js
+++ animals/animals.js
@@ -1,2 +1,17 @@
 import { animals } from '../data.js';
 import { findById } from '../utils.js';
+
+console.log('hello animals');
+console.log(window.location.search);
+
+const params = new URLSearchParams(window.location.search);
+console.log(params.get('id'));
+
+const animal = animals.find((item) => item.id === params.get('id'));
+console.log(animal);
+
+const name = document.getElementById('name');
+name.id = animal.type;
+const says = document.getElementById('says');
+name.textContent = animal.name;
+says.textContent = animal.says;
